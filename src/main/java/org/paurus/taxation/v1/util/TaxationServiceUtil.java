11a32e165fcd39923519a994a08bb8334694a4ed
Aljaz Hericko <aljaz-h@hotmail.com>
2024-02-15 16:58:33 +0100

    [A-001] Add minor code clarification javadoc

--- src/main/java/org/paurus/taxation/v1/util/TaxationServiceUtil.java
+++ src/main/java/org/paurus/taxation/v1/util/TaxationServiceUtil.java
@@ -10,14 +10,35 @@ public class TaxationServiceUtil {
         return playedAmount * decimalOdd;
     }
 
+    /**
+     * @param potentialWinAmount total amount the player would receive in case of winning
+     * @param playedAmount       amount the trader bet
+     * @param taxationRate       tax rate based on trader's country
+     * @return taxAmount the trader will have to pay in case of winning
+     * <p>
+     * Calculates the tax the player will have to pay in case of winning. Uses the taxation rate to determine whether to use general or winnings taxation
+     */
     public static double calculateTax(double potentialWinAmount, double playedAmount, TaxationRate taxationRate) {
         return switch (taxationRate.getTaxationType()) {
-            case GENERAL -> calculateTax(potentialWinAmount, taxationRate);
-            case WINNINGS -> calculateTax(potentialWinAmount - playedAmount, taxationRate);
+            case GENERAL -> calculateTax(potentialWinAmount, taxationRate.getTaxRate(), taxationRate.getTaxAmount());
+            case WINNINGS -> {
+                double taxableAmount = potentialWinAmount - playedAmount;
+                yield calculateTax(taxableAmount, taxationRate.getTaxRate(), taxationRate.getTaxAmount());
+            }
         };
     }
 
-    private static double calculateTax(double taxableAmount, TaxationRate taxationRate) {
-        return Math.min(taxableAmount * taxationRate.getTaxRate(), Math.min(taxableAmount, taxationRate.getTaxAmount()));
+    /**
+     * @param amountToTax amount based on which the taxation should be done
+     * @param taxRate     tax rate used for percentage based calculation
+     * @param taxAmount   tax amount for amount based calculation
+     * @return taxAmount the trader will have to pay in case of winning
+     * <p>
+     * The method determines, which tax option is more beneficial for the trader and uses it for calculating the tax amount.
+     */
+    private static double calculateTax(double amountToTax, Double taxRate, Double taxAmount) {
+        double rateTax = amountToTax * taxRate;
+        double amountTax = Math.min(amountToTax, taxAmount);
+        return Math.min(rateTax, amountTax);
     }
 }
